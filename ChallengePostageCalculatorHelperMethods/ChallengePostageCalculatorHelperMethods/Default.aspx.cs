e300411049a4cf5e54f78a57582ce68396cdd459
Gray-G <graygolden@gmail.com>
2017-03-31 12:26:37 -0700

    Initial Commit
    Commits starting project files and Default.aspx design layout.

---
+++ ChallengePostageCalculatorHelperMethods/ChallengePostageCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostageCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Method to retrieve textBox values and store them in variables
+
+
+
+        // Method to calculated textBox values times multiplier
+
+
+
+        // Method to display result
+
+
+
+    }
+}
